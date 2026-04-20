f516c16503db28dfe8468aeec91916a0de137bfb
alanNatanael <natanaelalan01@gmail.com>
2026-04-19 22:48:24 -0300

    Create ex011.js

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12 ) {
+        // Bom Dia!
+        img.src = 'manha.png'
+        document.body.style.background = '#fac562'
+    } else if (hora >= 12 && hora < 18 ) {
+        // Boa Tarde!
+        img.src =  'tarde.png'
+        document.body.style.background = '#D48043'
+    } else {
+        // Boa Noite!
+        img.src = 'noite.png'
+        document.body.style.background = '#06132a'
+    }
+}
