20e83ed3d71cf95cc5f5a03341325947b9e95d9d
alanNatanael <natanaelalan01@gmail.com>
2026-04-28 00:25:57 -0300

    refatorar estrutura de repetição no ambiente.js

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,5 +1,18 @@
+/* 
 var c = 1;
+do {
+   console.log(`Repetição: ${c}`);
+   c++ 
+} while (c <= 5);
+
 while (c <= 5) {
-    console.log(c);
+    console.log(`Repetição: ${c}`);
     c++
 }
+*/
+
+console.log('Vai começar...')
+for (var c = 1;c <= 5;c++) {
+    console.log(`Repetição: ${c}`);
+}
+console.log('Fim!')
