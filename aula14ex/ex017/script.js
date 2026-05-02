2616dc0ce17f6cd25c8d888bb40497a632a352e0
alanNatanael <natanaelalan01@gmail.com>
2026-05-01 23:01:24 -0300

    Mudança de repitição de while para for.

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -5,14 +5,21 @@ function tabuada() {
         window.alert('Por favor, digite um número!')
     } else {
         let n = Number(num.value)
-        let c = 1
+        /* let c = 1 */
         tab.innerHTML = ''
-        while (c <= 10) {
+
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+       /*  while (c <= 10) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n * c}`
             item.value = `tab${c}`
             tab.appendChild(item)
             c++
-        }
+        } */
     }
 }
